2e03fc2cc00ec8ace516df8a64ae48e2e29f92b2
Neelesh <neeleshparihar1902@gmail.com>
2026-07-09 01:22:02 +0530

    fix-vercel-build

--- backend/src/types/express/index.d.ts
+++ backend/src/types/express/index.d.ts
@@ -1,13 +1,13 @@
-import express from "express";
-//Extend the Express Request interface to include a user property
-//This allows us to attach user information to the request object after authentication
-declare module "express" { //this line means we are extending the existing module "express"
-    export interface Request { //this line means we are adding new properties to the existing Request interface
-        user?: {
-            userId: string;
-            email: string;
-        };
+// Extend the global Express namespace to include the user property on the Request interface
+declare global {
+  namespace Express {
+    interface Request {
+      user?: {
+        userId: string;
+        email: string;
+      };
     }
+  }
 }
 
-export { }; //This line is necessary to make this file a module, which allows us to use the declaration merging feature of TypeScript to extend the Express Request interface
+export {};
