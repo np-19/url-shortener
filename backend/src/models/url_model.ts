b5c54c6ab0d349d01e183bc51da7027625331dc5
Neelesh <neeleshparihar1902@gmail.com>
2026-07-23 21:23:12 +0530

    bug fixed in schema

--- backend/src/models/url_model.ts
+++ backend/src/models/url_model.ts
@@ -6,41 +6,42 @@ import { IUrl } from "../types/mongo_types.js";
 
 
 const urlSchema: Schema = new Schema<IUrl>(
-  {
-    originalUrl: {
-         type: String,
-        required: true
-     },
-    shortId: { 
-        type: String,
-        required: true,
-        unique: true 
+    {
+        originalUrl: {
+            type: String,
+            required: true
+        },
+        shortId: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        clicks: {
+            type: Number,
+            default: 0
+        },
+        userId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+            required: false
+        },
+        expiresAt: {
+            type: Date,
+            required: true
+        },
+        clicksByDate: {
+            type: Map,
+            of: Number,
+            default: {}
+        },
     },
-    clicks: {
-        type: Number,
-        default: 0
-    },
-    userId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: false
-    },
-    expiresAt: {
-        type: Date,
-        required: true
-    },
-    clicksByDate: {
-        type: Map,
-        of: Number,
-        default: {}
-    },
-  },
-  { timestamps: true }
+    { timestamps: true }
 );
 
-urlSchema.index({ "expiresAt": 1 }, { expireAfterSeconds: 0 }); //Auto-delete expired documents
+urlSchema.index({ expiresAt: 1 }, { expireAfterSeconds: 0 }); //Auto-delete expired documents
 urlSchema.index({ createdAt: -1 });
 urlSchema.index({ clicks: -1 });
+urlSchema.index({ clicksByDate: -1 });
 urlSchema.index({ userId: 1, _id: -1 });
 
 export const UrlModel = mongoose.model<IUrl>("Url", urlSchema);
